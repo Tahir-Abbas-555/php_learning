3ef16e62d8d63c5836da6df324fffecf82cb027e
Tahir Abbas Shaikh <143923259+Tahir-Abbas-555@users.noreply.github.com>
2024-12-19 07:01:31 +0000

    learn about how to declare a variable and use of variables

---
+++ Day2/day2.php
@@ -0,0 +1,17 @@
+<?php
+// Declaring variables
+$name = "Tahir";
+$age = 30;
+$price = 199.99;
+$is_active = true;
+
+// Printing variables
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Price: " . $price . "<br>";
+echo "Is Active: " . $is_active . "<br>";
+
+// Array example
+$colors = array("Red", "Green", "Blue");
+echo "Favorite Color: " . $colors[1]; // Outputs: Green
+?>
